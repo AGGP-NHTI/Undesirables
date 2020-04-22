aa71fb95e62743674e7e5cfb712bcdd0c9f9531f
thewesexpress <wesbrennan13@gmail.com>
2020-04-22 16:28:24 -0400

    updated HeroPawn

---
+++ Assets/Scripts/HeroPlayerController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class HeroPlayerController : PlayerController
+{
+
+    public override void Horizontal(float value)
+    {
+        HeroPawn HP = ((HeroPawn)PossesedPawn);
+        if (HP)
+        {
+            HP.Horizontal(value);
+        }
+    }
+
+
+}

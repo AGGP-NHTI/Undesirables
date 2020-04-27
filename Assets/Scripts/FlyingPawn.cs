b4acacfceb1e2569d4e7179db3f19bed30c40ae8
ArgentWaropod <62075349+ArgentWaropod@users.noreply.github.com>
2020-04-27 15:05:42 -0400

    Drone Updates

--- Assets/Scripts/FlyingPawn.cs
+++ Assets/Scripts/FlyingPawn.cs
@@ -2,78 +2,106 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public enum FlyingStates { IDLE, MOVING, GUN, BLADE, DAMAGED, DEAD }
+public enum FlyingStates { IDLE, MOVING, GUN, BLADEF, BLADER, DAMAGED, DEAD }
 
 public class FlyingPawn : DronePawn
 {
     public GameObject projectile;
     public GameObject spawnPoint;
+    public GameObject lowpoint;
+    public GameObject highpoint;
+    public GameObject hitbox;
+    public BoxCollider2D colbox;
     Rigidbody2D rb;
     FlyingStates states;
     public float flyHeight;
     float timer = 5.0f;
+    int nextState;
     bool isDead;
     public Animator flyAn;
     public GameObject ground;
     bool facingLeft;
     bool inAttack;
+    bool isFalling;
 
     // Start is called before the first frame update
     void Start()
     {
+        hitbox.SetActive(false);
         rb = gameObject.GetComponent<Rigidbody2D>();
         states = FlyingStates.IDLE;
         isDead = false;
         facingLeft = true;
+        isFalling = true;
         flyHeight = Random.Range(.75f, 3.0f);
+        colbox.enabled = true;
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
         timer -= Time.deltaTime;
-        
-        if (states == FlyingStates.MOVING)
+        if (!isDead)
         {
-                rb.velocity = new Vector2(0.5f, rb.velocity.y);
-        }
-        if (states == FlyingStates.GUN)
-        {
-
-        }
-        if (transform.position.y - ground.transform.position.y < flyHeight)
-        {
-            rb.velocity = new Vector2(rb.velocity.x, 0.5f);
-        }
-
-        if (timer < 0 && isDead != true && inAttack != true)
-        {
-            ResetTimer();
             if (states == FlyingStates.MOVING)
             {
-                flyAn.SetInteger("MoveState", 0);
-                Debug.Log("Changing States");
-                states = FlyingStates.IDLE;
+                rb.velocity = new Vector2(0.5f, rb.velocity.y);
             }
-            else
+            if (states == FlyingStates.GUN)
             {
-                flyAn.SetInteger("MoveState", 1);
-                Debug.Log("Changing States");
-                states = FlyingStates.MOVING;
-                Vector2 theScale = transform.localScale;
-                theScale.x *= -1;
-                facingLeft = !facingLeft;
-                transform.localScale = theScale;
+
+            }
+            if (states == FlyingStates.BLADEF)
+            {
+                rb.velocity = new Vector2(-0.5f, -2f);
+                if (gameObject.transform.position.y < lowpoint.transform.position.y)
+                {
+                    states = FlyingStates.BLADER;
+
+                }
+            }
+            if (states == FlyingStates.BLADER)
+            {
+                rb.velocity = new Vector2(-0.5f, 2f);
+
+                if (gameObject.transform.position.y > lowpoint.transform.position.y)
+                {
+                    states = FlyingStates.IDLE;
+                }
+            }
+            if (transform.position.y - ground.transform.position.y < flyHeight && isFalling)
+            {
+                rb.velocity = new Vector2(rb.velocity.x, 0.5f);
+            }
+
+            if (timer < 0 && inAttack != true)
+            {
+                if (states == FlyingStates.GUN)
+                {
+                    timer = 5.0f;
+                    flyAn.SetInteger("MoveState", 0);
+                    Debug.Log("Changing States");
+                    states = FlyingStates.IDLE;
+                }
+                else if (states == FlyingStates.IDLE)
+                {
+                    timer = 5.0f;
+                    flyAn.SetInteger("MoveState", 1);
+                    Debug.Log("Changing States");
+                    states = FlyingStates.MOVING;
+                    Vector2 theScale = transform.localScale;
+                    theScale.x *= -1;
+                    facingLeft = !facingLeft;
+                    transform.localScale = theScale;
+                }
+                else if (states == FlyingStates.MOVING)
+	            {
+                    timer = 3.0f;
+                    StartCoroutine(fire());
+                }
             }
         }
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            StartCoroutine(fire());
-        }
-    }
-    void ResetTimer()
-    {
-        timer = 5.0f;
+        
     }
     IEnumerator fire()
     {
@@ -91,5 +119,78 @@ public class FlyingPawn : DronePawn
         flyAn.ResetTrigger("GunAttack");
         inAttack = false;
         yield return null;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            if (collision.transform.position.x > gameObject.transform.position.x)
+            {
+                Vector2 theScale = transform.localScale;
+                theScale.x *= -1;
+                facingLeft = !facingLeft;
+                transform.localScale = theScale;
+            }
+            inAttack = true;
+            states = FlyingStates.BLADEF;
+            StartCoroutine(melee());
+        }
+    }
+
+    IEnumerator melee()
+    {
+        Debug.Log("Starting Coroutine");
+        isFalling = false;
+        flyAn.SetTrigger("BladeAttack");
+        yield return new WaitForSeconds(0.45f);
+        colbox.enabled = false;
+        hitbox.SetActive(true);
+        yield return new WaitForSeconds(1f);
+        hitbox.SetActive(false);
+        colbox.enabled = true;
+        yield return new WaitForSeconds(0.30f);
+        inAttack = false;
+        isFalling = true;
+        Debug.Log("Ending Coroutine");
+        yield return null;
+    }
+
+    IEnumerator deathAn()
+    {
+        flyAn.SetTrigger("IsDying");
+        isFalling = true;
+        yield return new WaitForSeconds(5f);
+        Destroy(gameObject);
+    }
+
+    protected override bool ProcessDamage(Actor Source, float Value, DamageEventInfo EventInfo = null, Controller Instigator = null)
+    {
+
+        health -= Value;
+        if (health <= 0)
+        {
+            if (_controller)
+            {
+                _controller.RequestSpectate();
+            }
+            Debug.Log(gameObject.name + " was killed by " + Instigator.playerName + " ripripripripripripripriprip");
+            isDead = true;
+            StartCoroutine(deathAn());
+        }
+
+        string DamageEventString = Source.ActorName + " " + EventInfo.DamageType.verb + " " + this.ActorName + " (" + Value.ToString() + " damage)";
+        if (Instigator)
+        {
+            DamageEventString = Instigator.playerName + " via " + DamageEventString;
+        }
+        else
+        {
+            DamageEventString = "The World via " + DamageEventString;
+        }
+        DAMAGELOG(DamageEventString);
+
+
+        return true;
     }
 }
--- Assets/Scripts/TankPawn.cs
+++ Assets/Scripts/TankPawn.cs
@@ -31,68 +31,75 @@ public class TankPawn : DronePawn
     {
         timer -= Time.deltaTime;
         secondTimer -= Time.deltaTime;
-        if (states == TankStates.WALKING)
+        if (!isDead)
         {
-            if (facingLeft)
-            {
-                rb.velocity = new Vector2(0.5f, rb.velocity.y);
-            }
-            else
-            {
-                rb.velocity = new Vector2(-0.5f, rb.velocity.y);
-            }
-        }
-        else if (states == TankStates.DAMAGED)
-        {
-
-        }
-        else if (states == TankStates.DEAD)
-        {
-
-        }
-        else
-        {
-
-        }
-        if (timer < 0 && isDead != true)
-        {
-            ResetTimer();
             if (states == TankStates.WALKING)
             {
-                tankAn.SetInteger("ChangeState", 0);
-                states = TankStates.IDLE;
+                if (facingLeft)
+                {
+                    rb.velocity = new Vector2(0.5f, rb.velocity.y);
+                }
+                else
+                {
+                    rb.velocity = new Vector2(-0.5f, rb.velocity.y);
+                }
+            }
+            else if (states == TankStates.DAMAGED)
+            {
+                StartCoroutine(isDamaged());
+            }
+            else if (states == TankStates.DEAD)
+            {
+                isDead = true;
             }
             else
             {
-                tankAn.SetInteger("ChangeState", 1);
-                states = TankStates.WALKING;
-                Vector2 theScale = transform.localScale;
-                theScale.x *= -1;
-                transform.localScale = theScale;
-                facingLeft = !facingLeft;
+
+            }
+            if (timer < 0 && isDead != true)
+            {
+                ResetTimer();
+                if (states == TankStates.WALKING)
+                {
+                    tankAn.SetInteger("ChangeState", 0);
+                    states = TankStates.IDLE;
+                }
+                else
+                {
+                    tankAn.SetInteger("ChangeState", 1);
+                    states = TankStates.WALKING;
+                    Vector2 theScale = transform.localScale;
+                    theScale.x *= -1;
+                    transform.localScale = theScale;
+                    facingLeft = !facingLeft;
+                }
+            }
+            Vector3 targ = target.transform.position;
+            targ.z = 0f;
+
+            Vector3 objectPos = turret.transform.position;
+            targ.x = targ.x - objectPos.x;
+            targ.y = targ.y - objectPos.y;
+
+            float angle = Mathf.Atan2(targ.y, targ.x) * Mathf.Rad2Deg;
+            if (facingLeft)
+            {
+                turret.transform.localScale = new Vector3(-1, 1, 1);
+            }
+            else
+            {
+                turret.transform.localScale = new Vector3(1, 1, 1);
+            }
+            turret.transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
+            if (secondTimer < 0)
+            {
+                secondTimer = 4.0f;
+                StartCoroutine(fire());
             }
         }
-        Vector3 targ = target.transform.position;
-        targ.z = 0f;
-
-        Vector3 objectPos = turret.transform.position;
-        targ.x = targ.x - objectPos.x;
-        targ.y = targ.y - objectPos.y;
-
-        float angle = Mathf.Atan2(targ.y, targ.x) * Mathf.Rad2Deg;
-        if (facingLeft)
+        if (health <= 0)
         {
-            turret.transform.localScale = new Vector3(-1, 1, 1);
-        }
-        else
-        {
-            turret.transform.localScale = new Vector3(1, 1, 1);
-        }
-            turret.transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
-        if (secondTimer < 0)
-        {
-            secondTimer = 4.0f;
-            StartCoroutine(fire());
+            isDead = true;
         }
     }
 
@@ -110,5 +117,53 @@ public class TankPawn : DronePawn
         }
 
         yield return null;
+    }
+
+    IEnumerator deathAn()
+    {
+        tankAn.SetTrigger("Killed");
+        yield return new WaitForSeconds(5f);
+        Destroy(gameObject);
+    }
+
+    IEnumerator isDamaged()
+    {
+        tankAn.SetTrigger("IsDamaged");
+        IgnoresDamage = true;
+        yield return new WaitForSeconds(2f);
+        IgnoresDamage = false;
+    }
+    protected override bool ProcessDamage(Actor Source, float Value, DamageEventInfo EventInfo = null, Controller Instigator = null)
+    {
+
+        health -= Value;
+        if (health <= 0)
+        {
+            if (_controller)
+            {
+                _controller.RequestSpectate();
+            }
+            Debug.Log(gameObject.name + " was killed by " + Instigator.playerName + " ripripripripripripripriprip");
+            isDead = true;
+            StartCoroutine(deathAn());
+        }
+        else
+        {
+            StartCoroutine(isDamaged());
+        }
+
+        string DamageEventString = Source.ActorName + " " + EventInfo.DamageType.verb + " " + this.ActorName + " (" + Value.ToString() + " damage)";
+        if (Instigator)
+        {
+            DamageEventString = Instigator.playerName + " via " + DamageEventString;
+        }
+        else
+        {
+            DamageEventString = "The World via " + DamageEventString;
+        }
+        DAMAGELOG(DamageEventString);
+
+
+        return true;
     }
 }

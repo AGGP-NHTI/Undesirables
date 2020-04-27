499b0f1b6965aa5402e82b5c56caa3283ce1c996
thewesexpress <wesbrennan13@gmail.com>
2020-04-27 15:00:39 -0400

    updated scripts

--- Assets/Scripts/HammerDamage.cs
+++ Assets/Scripts/HammerDamage.cs
@@ -4,13 +4,20 @@ using UnityEngine;
 
 public class HammerDamage : HeroPawn
 {
+    public float damageAmount = 10.0f;
+
     void Start()
     {
         
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        
+        Actor OtherActor = other.gameObject.GetComponentInParent<Actor>();
+        if (OtherActor)
+        {
+            OtherActor.TakeDamage(this, damageAmount, null, Owner);
+            
+        }
     }
 }

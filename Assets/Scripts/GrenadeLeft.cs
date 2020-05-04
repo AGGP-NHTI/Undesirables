7fe5a219f8d4c22825377b5441987f431316319a
ArgentWaropod <62075349+ArgentWaropod@users.noreply.github.com>
2020-05-04 13:33:10 -0400

    Revert "Test"
    This reverts commit 5324ceb2cf806b5b425e1f9a6347760ed1e0233e.

--- Assets/Scripts/GrenadeLeft.cs
+++ Assets/Scripts/GrenadeLeft.cs
@@ -4,26 +4,17 @@ using UnityEngine;
 
 public class GrenadeLeft : Actor
 {
-    public GameObject explosion;
+
     public float damageAmount = 50.0f;
     public float movementSpeed = 5f;
-    public float lifetime = 2.5f;
+    public float lifetime = 2f;
     public Rigidbody2D rb;
 
     void Start()
     {
         rb = gameObject.GetComponent<Rigidbody2D>();
         rb.velocity = (Vector2.up * movementSpeed) + (Vector2.left * movementSpeed);
-    }
-
-    private void Update()
-    {
-        lifetime = lifetime - Time.deltaTime;
-        if (lifetime <= 0f)
-        {
-            Instantiate(explosion, transform.position, Quaternion.identity);
-            Destroy(gameObject);
-        }
+        Destroy(gameObject, lifetime);
     }
 
     void OnCollisionEnter2D(Collision2D other)
@@ -41,7 +32,6 @@ public class GrenadeLeft : Actor
 
     public virtual void OnDeath()
     {
-        Instantiate(explosion, transform.position, Quaternion.identity);
         Destroy(gameObject);
     }
 }

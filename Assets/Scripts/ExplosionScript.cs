d3a334e95a0581b5474abba87bdbb0f2e33273c8
ArgentWaropod <62075349+ArgentWaropod@users.noreply.github.com>
2020-05-04 15:40:45 -0400

    Adds Grenade Explosion Audio

--- Assets/Scripts/ExplosionScript.cs
+++ Assets/Scripts/ExplosionScript.cs
@@ -4,8 +4,11 @@ using UnityEngine;
 
 public class ExplosionScript : MonoBehaviour
 {
+    AudioSource boom;
     void Start()
     {
+        boom = GetComponent<AudioSource>();
+        boom.Play();
         Destroy(gameObject, 1f);
     }
 
